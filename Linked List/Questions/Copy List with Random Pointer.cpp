7361885dd462e568838f668767586590750fe7d2
lunaticsash <pranjalakpr@gmail.com>
2026-02-28 11:26:02 +0530

    Copy List with Random Pointer

---
+++ Linked List/Questions/Copy List with Random Pointer.cpp
@@ -0,0 +1,66 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+
+public:
+
+    void insertNodes(Node*head){
+        Node* temp = head;
+        while(temp!=NULL){
+            Node* copyNode = new Node(temp->val);
+            copyNode->next = temp->next;
+            temp->next = copyNode;
+            temp = temp->next->next;
+        }
+    }
+
+    void connectRandom(Node* head){
+        Node* temp = head;
+
+        while(temp != NULL){
+            Node* copyNode = temp->next;
+
+            if(temp->random) copyNode->random = temp->random->next;
+            else copyNode->random = nullptr;
+            temp = temp->next->next;
+        }
+    }  
+
+    Node* connectNext(Node* head){
+        Node* dNode = new Node(-1);
+        Node* res = dNode;
+        Node* temp = head;
+
+        while( temp!= NULL){
+            res->next = temp->next;
+            temp->next = temp->next->next;
+
+            res = res->next;
+            temp = temp->next;
+        }
+
+        return dNode->next;
+    } 
+
+
+
+    Node* copyRandomList(Node* head) {
+        insertNodes(head);
+        connectRandom(head);
+        return connectNext(head);
+    }
+};
