bd40e10c30e1b2a8b3aad3fe276591bbf8d3bc1b
lunaticsash <pranjalakpr@gmail.com>
2026-03-02 21:48:34 +0530

    Traversal

---
+++ Trees/Binary Trees/Traversal.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+void preorder(struct Node *root)
+{
+    if (root == NULL)
+        return;
+
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void inorder(struct Node *root)
+{
+    if (root == NULL)
+        return;
+
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+void postorder(struct Node *root)
+{
+    if (root == NULL)
+        return;
+
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
+int main()
+{
+    struct Node *root = new Node(1);
+
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->right->left = new Node(4);
+
+    return 0;
+}
