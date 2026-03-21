2528873596a6ac039eced601dc70ec0baf696631
lunaticsash <pranjalakpr@gmail.com>
2026-03-21 08:50:49 +0530

    Climbing stairs

---
+++ DP (Dynamic Programming)/Climbing_Stairs.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int f(int ind, vector<int>& dp) {
+        if (ind == 0)
+            return 1;
+        if (ind == 1)
+            return 1;
+
+        if (dp[ind] != -1)
+            return dp[ind];
+
+        int left = f(ind - 1, dp);
+        int right = f(ind - 2, dp);
+
+        return dp[ind] = left+right;
+    }
+    int climbStairs(int n) {
+        vector<int> dp(n + 1, -1);
+        return f(n, dp);
+    }
+};
