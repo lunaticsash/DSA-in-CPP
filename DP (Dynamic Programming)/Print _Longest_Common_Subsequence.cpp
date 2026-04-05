0611d05be2846bb73978ce2ff07d52912d0488c4
lunaticsash <pranjalakpr@gmail.com>
2026-04-05 10:24:45 +0530

    Print Largest Common Subsequence

---
+++ DP (Dynamic Programming)/Print _Longest_Common_Subsequence.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Approach from Longest Common Subsequence, just applied backtracking on dp array for solving this
+// T.C = O(n+m)
+// S.C = O(n*m)
+int lcs3(string s, string t)
+{
+    int n = s.size();
+    int m = t.size();
+
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+    for (int i = 0; i <= n; i++)
+        dp[i][0] = 0;
+    for (int j = 0; j <= m; j++)
+        dp[0][j] = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if (s[i - 1] == t[j - 1])
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            else
+                dp[i][j] = max(dp[i - 1][j],
+                               dp[i][j - 1]);
+        }
+    }
+
+    int len = dp[n][m];
+    string ans = "";
+    for (int i = 0; i < len; i++)
+    {
+        ans += '$';
+    }
+
+    int index = len - 1;
+    int i = n, j = m;
+    while (i > 0 && j > 0)
+    {
+        if (s[i - 1] == t[j - 1])
+        {
+            ans[index] = s[i - 1];
+            index--;
+            i--, j--;
+        }
+        else if (dp[i - 1][j] > dp[i][j - 1])
+            i--;
+        else
+            j--;
+    }
+    cout << ans;
+
+    return 0;
+}
+
+int main()
+{
+    string s, t;
+
+    cout << "Enter first string: ";
+    cin >> s;
+
+    cout << "Enter second string: ";
+    cin >> t;
+
+    lcs3(s, t);
+
+    return 0;
+}
