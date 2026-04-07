b75d930876ebc8c87b41bc44e50fa66e6c33d358
lunaticsash <pranjalakpr@gmail.com>
2026-04-07 17:50:07 +0530

    Min Insertions to make a Palindrome

---
+++ DP (Dynamic Programming)/Min_Insertions_to_make_a_Palindrome.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int lcs3(string s, string t)
+{
+    int n = s.size();
+    int m = t.size();
+
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0)); //
+
+    for (int i = 0; i <= n; i++)
+        dp[i][0] = 0;
+    for (int j = 0; j <= m; j++)
+        dp[0][j] = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if (s[i - 1] == t[j - 1])
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            else
+                dp[i][j] = max(dp[i - 1][j],
+                               dp[i][j - 1]);
+        }
+    }
+
+    return dp[n][m];
+}
+
+int longestPalindromicSubsequence(string s)
+{
+    string t = s;
+    reverse(t.begin(), t.end());
+    return lcs3(s, t);
+}
+
+int minInsertionstomakeaPalindrome(string &str){
+    return str.size() - longestPalindromicSubsequence(str);
+}
