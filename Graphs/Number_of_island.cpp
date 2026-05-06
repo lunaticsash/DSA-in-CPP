f007e20595a528d6b4fcdd8fe3265b269a68db5d
lunaticsash <pranjalakpr@gmail.com>
2026-05-06 22:49:48 +0530

    Add implementation for Number of Islands problem using BFS in C++

---
+++ Graphs/Number_of_island.cpp
@@ -0,0 +1,109 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// All 8 directions
+//SC=O(N*M)  AS=O(N*M)
+//TS=O(N*M)  AS=O(N*M)
+class Solution
+{
+public:
+    void bfs(int row, int col, vector<vector<int>> &vis,
+             vector<vector<char>> &grid)
+    {
+        vis[row][col] = 1;
+        queue<pair<int, int>> q;
+        q.push({row, col});
+        int n = grid.size();
+        int m = grid[0].size();
+        while (!q.empty())
+        {
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+
+            // Traverse in the neighbours and mark them if its a land
+            for (int delrow = -1; delrow <= 1; delrow++)
+            {
+                for (int delcol = -1; delcol <= 1; delcol++)
+                {
+                    int nrow = row + delrow;
+                    int ncol = col + delcol;
+                    if (nrow >= 0 && ncol >= 0 && nrow < n && ncol < m &&
+                        grid[nrow][ncol] == '1' && !vis[nrow][ncol])
+                    {
+                        vis[nrow][ncol] = 1;
+                        q.push({nrow, ncol});
+                    }
+                }
+            }
+        }
+    }
+
+    int numIslands(vector<vector<char>> &grid)
+    {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        int cnt = 0;
+        for (int row = 0; row < n; row++)
+        {
+            for (int col = 0; col < m; col++)
+            {
+                if (!vis[row][col] && grid[row][col] == '1')
+                {
+                    cnt++;
+                    bfs(row, col, vis, grid);
+                }
+            }
+        }
+        return cnt;
+    }
+};
+
+// 4 Directional
+class Solution {
+
+public:
+    void bfs(int row, int col, vector<vector<int>>& vis,
+             vector<vector<char>>& grid) {
+        vis[row][col] = 1;
+        queue<pair<int, int>> q;
+        q.push({row, col});
+        int n = grid.size();
+        int m = grid[0].size();
+        int delrow[] = {-1, 0, 1, 0}; // 4-directional
+        int delcol[] = {0, 1, 0, -1};
+
+        while (!q.empty()) {
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+
+            for (int i = 0; i < 4; i++) {
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
+                if (nrow >= 0 && ncol >= 0 && nrow < n && ncol < m &&
+                    grid[nrow][ncol] == '1' && !vis[nrow][ncol]) {
+                    vis[nrow][ncol] = 1;
+                    q.push({nrow, ncol});
+                }
+            }
+        }
+    }
+
+    int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        int cnt = 0;
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < m; col++) {
+                if (!vis[row][col] && grid[row][col] == '1') {
+                    cnt++;
+                    bfs(row, col, vis, grid);
+                }
+            }
+        }
+        return cnt;
+    }
+};
