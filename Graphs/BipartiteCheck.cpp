c4caaf27ae35d16a4dd4b35a92fe92ff12086acb
lunaticsash <pranjalakpr@gmail.com>
2026-05-18 17:06:17 +0530

    Add implementation for Bipartite Graph Check using BFS

---
+++ Graphs/BipartiteCheck.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+
+public:
+    bool check(int start, int V, vector<int> adj[], int color[]){
+        queue<int>q;
+        q.push(start);
+        color[start] = 0;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            for(auto it: adj[node]){
+                //if the adjacent node is yet not coloured
+                // you will give the opposite color of node
+                if(color[it] == -1){
+                    color[it] = !color[node];
+                    q.push(it);
+                }
+
+                //is the adjacent guy having the same color as node then the graph is not bipartite
+                //some one did color it on the some other path
+                else if(color[it] == color[node]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    bool isBipartite(int V, vector<int> adj[]){
+        int color[V];
+        for(int i = 0; i < V; i++){
+            color[i] = -1;
+        }
+        for(int i = 0; i<V; i++){
+            if(color[i] == -1){
+                if(check(i,V,adj,color) == false) return false;
+            }
+        }
+        return true;
+    }
+
+};
