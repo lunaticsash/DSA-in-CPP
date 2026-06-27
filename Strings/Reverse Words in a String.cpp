4f7f4aac501d716223ab428d87e0c370575d1ff4
lunaticsash <pranjalakpr@gmail.com>
2026-06-27 11:34:54 +0530

    Add in-place solution for reversing words in a string with O(1) space complexity

--- Strings/Reverse Words in a String.cpp
+++ Strings/Reverse Words in a String.cpp
@@ -56,3 +56,50 @@ public:
 
 
 
+/*
+IN-PLACE SOLTUION WITH SC = O(1)
+class Solution {
+public:
+    string reverseWords(string s) {
+        int n = s.size();
+
+        // Step 1: reverse whole string
+        reverse(s.begin(), s.end());
+
+        int i = 0;
+        int j = 0;
+
+        // Step 2: remove extra spaces and store words in same string
+        while (i < n) {
+            // skip spaces
+            while (i < n && s[i] == ' ') {
+                i++;
+            }
+
+            // if this is not the first word, add one space before next word
+            if (j != 0 && i < n) {
+                s[j++] = ' ';
+            }
+
+            // copy current word
+            while (i < n && s[i] != ' ') {
+                s[j++] = s[i++];
+            }
+        }
+
+        // remove leftover extra characters
+        s.resize(j);
+
+        // Step 3: reverse every word individually
+        int start = 0;
+
+        for (int end = 0; end <= s.size(); end++) {
+            if (end == s.size() || s[end] == ' ') {
+                reverse(s.begin() + start, s.begin() + end);
+                start = end + 1;
+            }
+        }
+
+        return s;
+    }
+};*/
