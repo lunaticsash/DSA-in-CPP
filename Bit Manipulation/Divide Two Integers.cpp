ee93d8dff9d1134f403bafdd063fb3f85b363059
lunaticsash <pranjalakpr@gmail.com>
2026-07-02 12:25:25 +0530

    Dividing without using Divide (Divide Two Integers)

---
+++ Bit Manipulation/Divide Two Integers.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+//TC = O(logN), SC = O(1)
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        
+        if (dividend == INT_MIN && divisor == -1)
+            return INT_MAX;
+
+        long long dvd = abs((long long)dividend);
+        long long dvs = abs((long long)divisor);
+
+        long long ans = 0;
+
+        while (dvd >= dvs) {
+            int shift = 0;
+
+            while (dvd >= (dvs << (shift + 1))) {
+                shift++;
+            }
+
+            ans += (1LL << shift);
+            dvd -= (dvs << shift);
+        }
+
+        if ((dividend < 0) ^ (divisor < 0))
+            ans = -ans;
+
+        return (int)ans;
+    }
+};
