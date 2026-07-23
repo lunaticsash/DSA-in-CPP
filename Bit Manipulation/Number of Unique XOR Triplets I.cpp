c6e438ef1485fc04b0120c35407bd5af6d72bb03
lunaticsash <pranjalakpr@gmail.com>
2026-07-23 09:58:29 +0530

    Number of Unique XOR Triplets I

---
+++ Bit Manipulation/Number of Unique XOR Triplets I.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1)
+            return 1;
+        if (n == 2)
+            return 2;
+
+        int bits = 32 - __builtin_clz(n);
+
+        return 1 << bits;
+    }
+};
