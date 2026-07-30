b128a6635400b41787c1491bdd9079b03840592f
lunaticsash <pranjalakpr@gmail.com>
2026-07-30 14:13:37 +0530

    Better Approach with queue

--- Heaps/Hand of Straights.cpp
+++ Heaps/Hand of Straights.cpp
@@ -1,6 +1,46 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//Queue
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        queue<pair<int, int>> q;
+        int requiredCount = 0;
+        int pos = 0;
+        while(pos < nums.size()) {
+            if(pos != 0 && (nums[pos] - 1) != nums[pos - 1] && !q.empty()) {
+                return false;
+            }
+            int cur = nums[pos];
+            int count = 0;
+            while(pos < nums.size() && nums[pos] == cur) {
+                count++;
+                pos++;
+            }
+            if(count < requiredCount) {
+                return false;
+            } else if(count > requiredCount) {
+                q.push(make_pair(cur + k - 1, count - requiredCount));
+                requiredCount = count;
+            }
+            
+            if(!q.empty() && cur == q.front().first) {
+                requiredCount = requiredCount - q.front().second;
+                q.pop();
+            }
+        }
+
+        if(!q.empty()) {
+            return false;
+        }
+
+        return true;
+    }
+};
+
+//Map
 class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
