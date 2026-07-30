bb06119a9d32111547fa4f5b00debfd917d7e969
lunaticsash <pranjalakpr@gmail.com>
2026-07-30 12:49:52 +0530

    Improved SC of Merge k Sorted Lists

--- Linked List/Questions/Merge k Sorted Lists.cpp
+++ Linked List/Questions/Merge k Sorted Lists.cpp
@@ -9,7 +9,51 @@ struct ListNode
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
+// TC = O(NLOGK) where N is the total number of nodes in all lists and K is the number of lists.
+// SC = O(K) where K is the number of lists.
+class Solution
+{
+public:
+    struct cmp
+    {
+        bool operator()(ListNode *a, ListNode *b)
+        {
+            return a->val > b->val;
+        }
+    };
 
+    ListNode *mergeKLists(vector<ListNode *> &lists)
+    {
+        priority_queue<ListNode *, vector<ListNode *>, cmp> pq;
+
+        // Push the head of every non-empty list
+        for (ListNode *head : lists)
+        {
+            if (head)
+                pq.push(head);
+        }
+
+        ListNode dummy(0);
+        ListNode *tail = &dummy;
+
+        while (!pq.empty())
+        {
+            ListNode *node = pq.top();
+            pq.pop();
+
+            tail->next = node;
+            tail = tail->next;
+
+            if (node->next)
+                pq.push(node->next);
+        }
+
+        return dummy.next;
+    }
+};
+
+// TC = O(NLOGK) where N is the total number of nodes in all lists and K is the number of lists.
+// SC = O(K) + O(N)( for new ll )
 class Solution
 {
 public:
