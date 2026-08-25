0cb2b70096fa08b2515520b2a02718fa616efc9c
lunaticsash <pranjalakpr@gmail.com>
2026-08-25 10:13:29 +0530

    Smallest Missing Multiple of K

---
+++ Hashing/Smallest Missing Multiple of K.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int missingMultiple(vector<int>& nums, int k) {
+        vector<bool> freq(101,0);
+
+        for(int i=0;i<nums.size();i++)
+        freq[nums[i]] = 1;
+
+        int i = k;
+        while(i <= 100) {
+            if(!freq[i])
+            return i;
+            i += k;
+        }
+        return ((100 / k) + 1) * k;
+    }
+};
