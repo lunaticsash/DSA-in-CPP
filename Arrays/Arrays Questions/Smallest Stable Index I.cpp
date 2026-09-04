89927449e395d5ea85a64441301d632fa3968e08
lunaticsash <pranjalakpr@gmail.com>
2026-09-04 11:31:02 +0530

    Smallest Stable Index I

---
+++ Arrays/Arrays Questions/Smallest Stable Index I.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        vector<int> suffixMin(n);
+
+        suffixMin[n - 1] = nums[n - 1];
+
+        for (int i = n - 2; i >= 0; i--) {
+            suffixMin[i] = min(nums[i], suffixMin[i + 1]);
+        }
+
+        int maxi = INT_MIN;
+
+        for (int i = 0; i < n; i++) {
+            maxi = max(maxi, nums[i]);
+
+            int diff = maxi - suffixMin[i];
+
+            if (diff <= k) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
